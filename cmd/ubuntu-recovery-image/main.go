aae6d9158e07a5d6af5670f2eb8fa898b1c408ee
Woodrow Shen <woodrow.shen@canonical.com>
2017-04-10 15:26:55 +0800

    [Recovery] ubuntu-recovery-image fail with encryptfs

--- cmd/ubuntu-recovery-image/main.go
+++ cmd/ubuntu-recovery-image/main.go
@@ -59,7 +59,13 @@ func setupLoopDevice(recoveryOutputFile string, recoveryNR string, label string)
 	// image size should be larger than or equal to base image. or the gpt table copy would failed
 	imageSize := MaxInt64(int64(recoverySize+20)*1024*1024, basefilest.Size())
 	err = syscall.Fallocate(int(outputfile.Fd()), 0, 0, imageSize)
-	rplib.Checkerr(err)
+	if err != nil {
+		log.Printf("syscall.Fallocate may not support!! Try to use dd to allocate")
+		rplib.DD("/dev/zero", recoveryOutputFile, "bs=1M", fmt.Sprintf("count=%d", imageSize/1024/1024), "conv=notrunc")
+		if _, err := os.Stat(recoveryOutputFile); os.IsNotExist(err) {
+			rplib.Checkerr(err)
+		}
+	}
 
 	//copy partition table
 	log.Printf("Copy partitition table")
