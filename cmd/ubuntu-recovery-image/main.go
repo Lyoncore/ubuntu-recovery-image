6844ca22cacaa1350c39914f5bdd2a163e369cc0
Woodrow Shen <woodrow.shen@canonical.com>
2017-04-12 12:12:50 +0800

    Add conditions to avoid worst case.

--- cmd/ubuntu-recovery-image/main.go
+++ cmd/ubuntu-recovery-image/main.go
@@ -61,6 +61,12 @@ func setupLoopDevice(recoveryOutputFile string, recoveryNR string, label string)
 	err = syscall.Fallocate(int(outputfile.Fd()), 0, 0, imageSize)
 	if err != nil {
 		log.Print(err)
+	}
+	fileInfo, err := os.Stat(recoveryOutputFile)
+	if os.IsNotExist(err) {
+		rplib.Checkerr(err)
+	}
+	if fileInfo.Size() < imageSize {
 		log.Print("syscall.Fallocate may not be supported!! Try to use dd to allocate")
 		rplib.DD("/dev/zero", recoveryOutputFile, "bs=1M", fmt.Sprintf("count=%d", imageSize/1024/1024), "conv=notrunc")
 		if _, err := os.Stat(recoveryOutputFile); os.IsNotExist(err) {
